dc037ad0e2ee06bbdc7be8d007676d00f9a43137
anna-hein25 <156244359+anna-hein25@users.noreply.github.com>
2024-03-18 18:50:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,23 +5,20 @@ GGR472 LAB 4: Incorporating GIS Analysis into web maps using Turf.js
 /*--------------------------------------------------------------------
 Step 1: INITIALIZE MAP
 --------------------------------------------------------------------*/
-// Define access token
-mapboxgl.accessToken = 'pk.eyJ1IjoiYW5uYS1oZWluMSIsImEiOiJjbHMyOWllNW8wa2J3MmpsZHM1eHk0b3oxIn0.9g1JErkZTD4sg70-swx-YQ'; //****ADD YOUR PUBLIC ACCESS TOKEN*****
+// defining access token
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW5uYS1oZWluMSIsImEiOiJjbHMyOWllNW8wa2J3MmpsZHM1eHk0b3oxIn0.9g1JErkZTD4sg70-swx-YQ';
 
-// Initialize map and edit to your preference
+// Initializing map
 const map = new mapboxgl.Map({
     container: 'map', // container id in HTML
-    style: 'mapbox://styles/anna-hein1/cltq8azbv024r01qp54by1bkc',  // ****ADD MAP STYLE HERE *****
-    center: [-79.39, 43.65],  // starting point, longitude/latitude
-    zoom: 12 // starting zoom level
+    style: 'mapbox://styles/anna-hein1/cltq8azbv024r01qp54by1bkc',
+    center: [-79.38, 43.72],  // starting point, longitude/latitude
+    zoom: 10.5 // starting zoom level
 });
 
 /*--------------------------------------------------------------------
 Step 2: VIEW GEOJSON POINT DATA ON MAP
 --------------------------------------------------------------------*/
-//HINT: Create an empty variable
-//      Use the fetch method to access the GeoJSON from your online repository
-//      Convert the response to JSON format and then store the response in your new variable
 
 let collisiongeojson;
 
@@ -35,10 +32,6 @@ fetch('https://raw.githubusercontent.com/anna-hein25/GGR472-lab4/ed29c9a264739fa
 /*--------------------------------------------------------------------
     Step 3: CREATE BOUNDING BOX AND HEXGRID
 --------------------------------------------------------------------*/
-//HINT: All code to create and view the hexgrid will go inside a map load event handler
-//      First create a bounding box around the collision point data then store as a feature collection variable
-//      Access and store the bounding box coordinates as an array variable
-//      Use bounding box coordinates as argument in the turf hexgrid function
 
 map.on('load', () => {
 
@@ -48,20 +41,18 @@ map.on('load', () => {
 
     bboxgeojson = {
         'type': 'FeatureCollection',
-        'features': [bbox]
+        'features': [bboxscaled]
     };
 
     let bboxcoords = [bboxscaled.geometry.coordinates[0][0][0],
     bboxscaled.geometry.coordinates[0][0][1],
     bboxscaled.geometry.coordinates[0][2][0],
     bboxscaled.geometry.coordinates[0][2][1],];
-    let hexgeojson = turf.hexGrid(bboxcoords, 0.5, { units: 'kilometers' });
+    let hexgeojson = turf.hexGrid(bboxcoords, 0.3, { units: 'kilometers' });
 
     /*--------------------------------------------------------------------
     Step 4: AGGREGATE COLLISIONS BY HEXGRID
     --------------------------------------------------------------------*/
-    //HINT: Use Turf collect function to collect all '_id' properties from the collision points data for each heaxagon
-    //      View the collect output in the console. Where there are no intersecting points in polygons, arrays will be empty
 
     let collishex = turf.collect(hexgeojson, collisiongeojson, '_id', 'values');
 
@@ -75,15 +66,9 @@ map.on('load', () => {
         }
     });
 
-    // /*--------------------------------------------------------------------
-    // Step 5: FINALIZE YOUR WEB MAP
-    // --------------------------------------------------------------------*/
-    //HINT: Think about the display of your data and usability of your web map.
-    //      Update the addlayer paint properties for your hexgrid using:
-    //        - an expression
-    //        - The COUNT attribute
-    //        - The maximum number of collisions found in a hexagon
-    //      Add a legend and additional functionality including pop-up windows
+    /*--------------------------------------------------------------------
+    Step 5: FINALIZE YOUR WEB MAP
+    --------------------------------------------------------------------*/
 
     map.addSource('collision-hex', {
         type: 'geojson',
@@ -95,12 +80,14 @@ map.on('load', () => {
         'type': 'fill',
         'source': 'collision-hex',
         'paint': {
-            'fill-color': [
+            'fill-color': [ // the colors used here are assigned to certain values and is displayed on the legend
                 'step',
                 ['get', 'COUNT'],
-                '#800026',
-                10, '#bd0026',
-                25, '#e31a1c'
+                '#fd8d3c', // colors assigned to any values < first step
+                5, '#fc4e2a', // colors assigned to values >= each step
+                10, '#e31a1c',
+                16, '#bd0026',
+                22, '#800026'
             ],
             'fill-opacity': 0.5,
             'fill-outline-color': "white"
@@ -108,9 +95,14 @@ map.on('load', () => {
     });
 });
 
-map.on('click', 'collis-hex-fill', (e) => {
-    new mapboxgl.Popup()
-    .setLngLat(e.lngLat)
-    .setHTML("<b>Collision count:<b>" + e.features[0].properties.COUNT)
-    .addTo(map);
-})
+
+// defining the popup window
+// allows users to click a hexagon and the exact number of pedestrian collisions occurred in that area
+map.on('click', 'collision-hex-fill', (e) => {
+    if (e.features.length > 0) {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML("<b>Pedestrian Collision count:</b> " + e.features[0].properties.COUNT)
+            .addTo(map);
+    }
+});
